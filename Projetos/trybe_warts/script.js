ba7b32f4cd3418e770fbb3f52df1f5dd3c082e71
jarana-2020 <julioaranabr@yahoo.com.br>
2021-08-16 20:56:25 -0300

    Adicionada Pasta de Fonte Temática

--- Projetos/trybe_warts/script.js
+++ Projetos/trybe_warts/script.js
@@ -1,153 +1,74 @@
-/* eslint-disable no-shadow */
-/* eslint-disable no-constant-condition */
-function alterColor() {
-  const getTasks = document.querySelectorAll('.task');
-  for (let index = 0; index < getTasks.length; index += 1) {
-    getTasks[index].classList.remove('selected');
-    getTasks[index].style.backgroundColor = 'white';
+const login = document.querySelector('#login');
+const password = document.querySelector('#password');
+const buttonNav = document.querySelector('#button-nav');
+const checkBox = document.getElementById('agreement');
+const submitButton = document.querySelector('#submit-btn');
+const counter = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
+const form = document.querySelector('#evaluation-form');
+const inputName = document.querySelector('#input-name');
+const inputSurname = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const house = document.querySelector('#house');
+const familyValue = document.querySelectorAll('.familyValue');
+const rateValue = document.querySelectorAll('.rate');
+const subjectValue = document.querySelectorAll('.subject');
+
+buttonNav.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (login.value !== 'tryber@teste.com' && password.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
-}
+});
 
-function empityTasks() {
-  const getOl = document.getElementById('lista-tarefas');
-  while (getOl.firstChild) {
-    getOl.removeChild(getOl.firstChild);
+checkBox.addEventListener('click', () => {
+  if (checkBox.checked) {
+    submitButton.removeAttribute('disabled');
+  } else {
+    submitButton.disabled = true;
   }
-  localStorage.removeItem('taskList');
-}
-function removeCompleted() {
-  const getOl = document.getElementById('lista-tarefas');
-  const getLi = document.querySelectorAll('.completed');
-  for (let index = 0; index < getLi.length; index += 1) {
-    getOl.removeChild(getLi[index]);
-  }
-}
+});
 
-// eslint-disable-next-line max-lines-per-function
-function addTask() {
-  // eslint-disable-next-line sonarjs/no-duplicate-string
-  const getOl = document.querySelector('#lista-tarefas');
-  const getButtonTask = document.querySelector('#criar-tarefa');
-  const getInput = document.querySelector('#texto-tarefa');
-  // eslint-disable-next-line prefer-arrow-callback
-  // eslint-disable-next-line func-names
-  // eslint-disable-next-line max-lines-per-function
-  getButtonTask.addEventListener('click', () => {
-    const createLi = document.createElement('li');
-    createLi.className = 'task';
-    createLi.innerHTML = getInput.value;
-    createLi.addEventListener('click', (event) => {
-      alterColor();
-      event.target.classList.add('selected');
-      // eslint-disable-next-line no-param-reassign
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-      // eslint-disable-next-line no-param-reassign
-    });
-    createLi.addEventListener('dblclick', (event) => {
-      if (event.target.classList.contains('completed')) {
-        event.target.classList.remove('completed');
-      } else {
-        event.target.classList.add('completed');
-      }
-    });
-    getOl.appendChild(createLi);
-    getInput.value = '';
-  });
-}
-addTask();
+textArea.addEventListener('keyup', () => {
+  const size = 500 - textArea.value.length;
+  counter.innerHTML = size;
+});
 
-function removeTasks() {
-  const getButtonRemove = document.querySelector('#apaga-tudo');
-  getButtonRemove.addEventListener('click', () => {
-    empityTasks();
-  });
-}
-removeTasks();
-
-function btnRemoveCompleted() {
-  const getButton = document.getElementById('remover-finalizados');
-  getButton.addEventListener('click', () => {
-    removeCompleted();
-  });
-}
-btnRemoveCompleted();
-
-function saveTasks() {
-  const taskList = document.querySelector('#lista-tarefas');
-  const getButtonSaveTasks = document.getElementById('salvar-tarefas');
-  getButtonSaveTasks.addEventListener('click', () => {
-    localStorage.setItem('taskList', taskList.innerHTML);
-  });
-}
-saveTasks();
-
-// eslint-disable-next-line max-lines-per-function
-function setTasksSaved() {
-  const taskList = document.querySelector('#lista-tarefas');
-  if (localStorage.getItem('taskList') !== '') {
-    taskList.innerHTML = localStorage.getItem('taskList');
-  }
-  const getTasks = document.querySelectorAll('.task');
-  for (let index = 0; index < getTasks.length; index += 1) {
-    // eslint-disable-next-line sonarjs/no-identical-functions
-    getTasks[index].addEventListener('click', (event) => {
-      alterColor();
-      const getEvent = event.target;
-      getEvent.classList.add('selected');
-      getEvent.style.backgroundColor = 'rgb(128, 128, 128)';
-      // eslint-disable-next-line no-shadow
-      // eslint-disable-next-line sonarjs/no-identical-functions
-      getTasks.addEventListener('dblclick', (event) => {
-        if (event.target.classList.contains('completed')) {
-          event.target.classList.remove('completed');
-        } else {
-          event.target.classList.add('completed');
-        }
-      });
-    });
-  }
-}
-setTasksSaved();
-
-// As duas funções abaixo tiveram uma consulta ao código do amigo ISRAEL-NAZARETH Turma -B da Trybe.
-
-function moveUp() {
-  const getButtonUp = document.getElementById('mover-cima');
-  getButtonUp.addEventListener('click', () => {
-    const getSelected = document.querySelector('.selected');
-    const getTasks = document.querySelectorAll('.task');
-    if (getSelected === null) {
-      // eslint-disable-next-line sonarjs/no-duplicate-string
-      alert('Nenhuma tarefa foi selecionada');
-    } else if (getSelected !== getTasks[0]) {
-      getSelected.parentNode.insertBefore(getSelected, getSelected.previousElementSibling);
+function validateSubject() {
+  let list = '';
+  for (let index = 0; index < subjectValue.length; index += 1) {
+    if (subjectValue[index].checked) {
+      list += `${subjectValue[index].value}, `;
     }
-  });
+  }
+  return list;
 }
-moveUp();
 
-function moveDown() {
-  const getButtonDown = document.getElementById('mover-baixo');
-  getButtonDown.addEventListener('click', () => {
-    const getSelected = document.querySelector('.selected');
-    if (getSelected === null) {
-      alert('Nenhuma tarefa foi selecionada');
-    } else if (getSelected !== getSelected.parentNode.lastChild) {
-      getSelected.parentNode.insertBefore(getSelected.nextElementSibling, getSelected);
+function validateRate() {
+  for (let index = 0; index < rateValue.length; index += 1) {
+    if (rateValue[index].checked) {
+      return rateValue[index].value;
     }
-  });
+  }
 }
-moveDown();
 
-function removeTaskSelected() {
-  const buttonRemove = document.getElementById('remover-selecionado');
-  buttonRemove.addEventListener('click', () => {
-    const getSelected = document.querySelector('.selected');
-    if (getSelected === null) {
-      alert('Nenhuma tarefa foi selecionada');
-    } else {
-      getSelected.parentNode.removeChild(getSelected);
+function validateFamily() {
+  for (let index = 0; index < familyValue.length; index += 1) {
+    if (familyValue[index].checked) {
+      return familyValue[index].value;
     }
-  });
+  }
 }
-removeTaskSelected();
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  form.innerHTML = `<p>Nome: ${inputName.value} ${inputSurname.value}</p>`;
+  form.innerHTML += `<p>Email: ${inputEmail.value}</p>`;
+  form.innerHTML += `<p>Casa: ${house.value}</p>`;
+  form.innerHTML += `<p>Família: ${validateFamily()}</p>`;
+  form.innerHTML += `<p>Matérias: ${validateSubject()} </p>`;
+  form.innerHTML += `<p>Avaliação: ${validateRate()} </p>`;
+  form.innerHTML += `<p>Observações: ${textArea.value} </p>`;
+});
